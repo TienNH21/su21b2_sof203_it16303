1075a0236ad4036317cbc6066e74af8905d9f5e3
HoangTien339 <tiennguyenhoang339@gmail.com>
2021-07-14 11:17:52 +0700

    Lesson 4: JMenu

--- src/main/java/lesson3/DemoTimer.java
+++ src/main/java/lesson3/DemoTimer.java
@@ -22,7 +22,10 @@ public class DemoTimer extends javax.swing.JFrame {
      */
     public DemoTimer() {
         initComponents();
-        
+
+        this.progressBar.setMinimum(0);
+        this.progressBar.setMaximum(100);
+
         DemoTimer that = this;
         ActionListener listener = new ActionListener() {
             @Override
@@ -30,14 +33,28 @@ public class DemoTimer extends javax.swing.JFrame {
                 int result = JOptionPane.showConfirmDialog(that,
                     "Tiếp tục chạy?", "Confirm", JOptionPane.YES_NO_OPTION);
 
-                if (result == 1) {
+                int value = that.progressBar.getValue();
+                if (value == 100) {
                     that.timer.stop();
+                    that.btnStart.setEnabled(true);
+                    JOptionPane.showMessageDialog(that, "Kết thúc!");
+                    return ;
                 }
+
+                if (result == 1) {
+                    // Chọn No
+                    that.timer.stop();
+                    that.btnStart.setEnabled(true);
+                    that.progressBar.setValue(100);
+                    return;
+                }
+                
+                that.progressBar.setValue(value + 10);
             }
         };
 
         this.timer = new Timer(2000, listener);
-        this.timer.start();
+//        this.timer.start();
     }
 
     /**
@@ -50,18 +67,45 @@ public class DemoTimer extends javax.swing.JFrame {
     private void initComponents() {
 
         mainPanel = new javax.swing.JPanel();
+        progressBar = new javax.swing.JProgressBar();
+        jLabel1 = new javax.swing.JLabel();
+        btnStart = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Progress");
+
+        btnStart.setText("Start");
+        btnStart.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                btnStartActionPerformed(evt);
+            }
+        });
 
         javax.swing.GroupLayout mainPanelLayout = new javax.swing.GroupLayout(mainPanel);
         mainPanel.setLayout(mainPanelLayout);
         mainPanelLayout.setHorizontalGroup(
             mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 388, Short.MAX_VALUE)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, mainPanelLayout.createSequentialGroup()
+                .addContainerGap(84, Short.MAX_VALUE)
+                .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(btnStart)
+                    .addGroup(mainPanelLayout.createSequentialGroup()
+                        .addComponent(jLabel1)
+                        .addGap(18, 18, 18)
+                        .addComponent(progressBar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)))
+                .addGap(86, 86, 86))
         );
         mainPanelLayout.setVerticalGroup(
             mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 288, Short.MAX_VALUE)
+            .addGroup(mainPanelLayout.createSequentialGroup()
+                .addGap(97, 97, 97)
+                .addGroup(mainPanelLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jLabel1)
+                    .addComponent(progressBar, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addComponent(btnStart)
+                .addContainerGap(124, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -83,6 +127,12 @@ public class DemoTimer extends javax.swing.JFrame {
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void btnStartActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnStartActionPerformed
+        this.progressBar.setValue(0);
+        this.timer.start();
+        this.btnStart.setEnabled(false);
+    }//GEN-LAST:event_btnStartActionPerformed
 
     /**
      * @param args the command line arguments
@@ -120,6 +170,9 @@ public class DemoTimer extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton btnStart;
+    private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel mainPanel;
+    private javax.swing.JProgressBar progressBar;
     // End of variables declaration//GEN-END:variables
 }
